b17412c36e6812a44ff98ab46c8f489b3eb3c7b5
Dorota Jarecka <djarecka@gmail.com>
2017-06-24 10:07:06 -0700

    changing paths in the test file

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 
+import os
+
+Dir_path = os.path.abspath(__file__ + "/../")
+
 def test_gitannex():
-    with open("git_annex.txt") as f:
+    with open(os.path.join(Dir_path, "git_annex.txt")) as f:
         con = f.read()
 
     assert "this should be saved on s3 using git annex" in con
 
 def test_gitonly():
-    with open("git_file.txt") as f:
+    with open(os.path.join(Dir_path,"git_file.txt")) as f:
         con = f.read()
 
     assert "this will be just a git file" in con
